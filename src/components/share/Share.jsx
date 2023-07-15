a01f8f4f09958014d3336cf701cda8b5fda8ffc3
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-07-16 06:09:48 +0700

    post with image

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -26,15 +26,15 @@ const Share = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation(
-    (newPost) => {
-      return makeRequest.post("/posts", newPost);
-    },
-    {
-      onSuccess: () => {
-        // Invalidate and refetch
-        queryClient.invalidateQueries(["posts"]);
+      (newPost) => {
+        return makeRequest.post("/posts", newPost);
       },
-    }
+      {
+        onSuccess: () => {
+          // Invalidate and refetch
+          queryClient.invalidateQueries(["posts"]);
+        },
+      }
   );
 
   const handleClick = async (e) => {
@@ -42,59 +42,57 @@ const Share = () => {
     let imgUrl = "";
     if (file) imgUrl = await upload();
     mutation.mutate({ desc, img: imgUrl });
-    setDesc("");
-    setFile(null);
   };
 
   return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <div className="left">
-            <img src={"/upload/" + currentUser.profilePic} alt="" />
-            <input
-              type="text"
-              placeholder={`What's on your mind ${currentUser.name}?`}
-              onChange={(e) => setDesc(e.target.value)}
-              value={desc}
-            />
+      <div className="share">
+        <div className="container">
+          <div className="top">
+            <div className="left">
+              <img src={"/upload/" + currentUser.profilePic} alt="" />
+              <input
+                  type="text"
+                  placeholder={`What's on your mind ${currentUser.name}?`}
+                  onChange={(e) => setDesc(e.target.value)}
+                  value={desc}
+              />
+            </div>
+            <div className="right">
+              {file && (
+                  <img className="file" alt="" src={URL.createObjectURL(file)} />
+              )}
+            </div>
           </div>
-          <div className="right">
-            {file && (
-              <img className="file" alt="" src={URL.createObjectURL(file)} />
-            )}
-          </div>
-        </div>
-        <hr />
-        <div className="bottom">
-          <div className="left">
-            <input
-              type="file"
-              id="file"
-              style={{ display: "none" }}
-              onChange={(e) => setFile(e.target.files[0])}
-            />
-            <label htmlFor="file">
+          <hr />
+          <div className="bottom">
+            <div className="left">
+              <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={(e) => setFile(e.target.files[0])}
+              />
+              <label htmlFor="file">
+                <div className="item">
+                  <img src={Image} alt="" />
+                  <span>Add Image</span>
+                </div>
+              </label>
               <div className="item">
-                <img src={Image} alt="" />
-                <span>Add Image</span>
+                <img src={Map} alt="" />
+                <span>Add Place</span>
               </div>
-            </label>
-            <div className="item">
-              <img src={Map} alt="" />
-              <span>Add Place</span>
+              <div className="item">
+                <img src={Friend} alt="" />
+                <span>Tag Friends</span>
+              </div>
             </div>
-            <div className="item">
-              <img src={Friend} alt="" />
-              <span>Tag Friends</span>
+            <div className="right">
+              <button onClick={handleClick}>Share</button>
             </div>
-          </div>
-          <div className="right">
-            <button onClick={handleClick}>Share</button>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
