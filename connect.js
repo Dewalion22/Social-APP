ebefd1bf8b8e83c0435b665fc2921426b5184910
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-08-12 18:31:58 +0700

    update connect

--- connect.js
+++ connect.js
@@ -1,12 +1,13 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"fadel123",
-    port:3309,
-    database:"social",
+    connectionLimit : 10,
+    host:"202.73.26.166",
+    user:"socialap_fadel",
+    password:"fadel202020",
+    port:3306,
+    database:"socialap_social-apps",
 
-})
+});
 
 
